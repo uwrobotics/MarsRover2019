bceeac29ee04d489af496107947547540586b396
Jordan Juravsky <your_email@domainname.com>
2018-10-24 21:15:23 -0400

    Making directory and project outline

---
+++ Workspace/src/cost_map/cost_map_node.cpp
@@ -0,0 +1,20 @@
+// Jordan Juravsky, Hanz Vora, Ayush Ghosh, Nigel Tims
+
+/*
+
+Ok what we need to do
+
+ros initialization stuff - i.e. setting up the subscribers, publishers, etc.
+
+main loop{
+	
+	grab subsciber data - get new location changes and object camera stuff
+
+	localization update - rotate and translate cost map
+
+	object update - add new objects to map
+
+	publish refreshed map. 
+}
+
+*/
