96048f8f310b02716df37b8f4f792e476a3a00e3
OmarAlashqar <o.ashqar@outlook.com>
2018-11-13 22:55:18 -0500

    refactor

--- Workspace/src/ik/src/Roboarm.cpp
+++ Workspace/src/ik/src/Roboarm.cpp
@@ -1,42 +1,50 @@
-#include "../include/ik/Roboarm.h"
 #include <iostream>
 #include <cmath>
+#include "../include/ik/Roboarm.h"
+
 using namespace std;
 
-Roboarm::Roboarm(float l_s, float l_e, float l_w,float shoulder, float elbow, float wrist) :
-    shoulder_len(l_s), elbow_len(l_e), wrist_len(l_w){
+Roboarm::Roboarm(float l1, float l2, float l3, float a1, float a2, float a3) :
+  lengthLink1(l1), lengthLink2(l2), lengthLink3(l3),
+  angleLink1(a1), angleLink2(a2), angleLink3(a3) {
 
-    shoulder_pos =shoulder;
-    elbow_pos = elbow;
-    wrist_pos = wrist;
-
-    shoulder_vel = 0;
-    elbow_vel = 0;
-    wrist_vel = 0;
-
-
+  velocityLink1 = 0;
+  velocityLink2 = 0;
+  velocityLink3 = 0;
 }
 
-void Roboarm::calc_velocities(float ee_x, float ee_y, float ee_phi){
-    float c1 = cos(shoulder_pos);
-    float s1 = sin(shoulder_pos);
-    float c12 = cos(shoulder_pos+elbow_pos);
-    float s12 = sin(shoulder_pos+elbow_pos);
-    float c123 = cos(shoulder_pos+elbow_pos+wrist_pos);
-    float s123 = sin(shoulder_pos+elbow_pos+wrist_pos);
-    std::cout<<ee_y*(sin(ee_phi));
-    float l1=shoulder_len;
-    float l2 =elbow_len;
-    float l3 = wrist_len;
+void Roboarm::calc_velocities(float endEffectorVX, float endEffectorVY, float endEffectorPhi) {
+  float c1 = cos(angleLink1);
+  float s1 = sin(angleLink1);
 
-    shoulder_vel = l2*c12*ee_x + l2*s12*ee_y + l2*l3*(c12*s123-s12*c123)*ee_phi;
-    elbow_vel = (-l1*c1 -l2*c12)*ee_x - (l1*s1 + l2*s12)*ee_y - (l1*l3*(c1*s123-s1*c123)+l2*l3*(c12*s123-s12*c123))*ee_phi;
-    wrist_vel = l1*c1*ee_x +l1*s1*ee_y + (l1*l2*(c1*s12-s1*c12)+l1*l3*(c1*s123-s1*c123))*ee_phi;
+  float c12 = cos(angleLink1 + angleLink2);
+  float s12 = sin(angleLink1 + angleLink2);
 
+  float c123 = cos(angleLink1 + angleLink2 + angleLink3);
+  float s123 = sin(angleLink1 + angleLink2 + angleLink3);
+    
+  float l1 = lengthLink1;
+  float l2 = lengthLink2;
+  float l3 = lengthLink3;
 
+  float eX = endEffectorVX;
+  float eY = endEffectorVY;
+  float eP = endEffectorPhi;
 
+  velocityLink1 =
+      (l2 * c12 * eX)
+    + (l2 * s12 * eY)
+    + (l2 * l3 * (c12 * s123 - s12 * c123) * eP);
+    
+  velocityLink2 =
+      (-l1 * c1 - l2 * c12) * eX
+    - (l1 * s1 + l2 * s12) * eY
+    - (l1 * l3 * (c1 * s123 - s1 * c123) + l2 * l3 * (c12 * s123 - s12 * c123)) * eP;
+
+  velocityLink3 =
+      (l1 * c1 * eX)
+    + (l1 * s1 * eY)
+    + (l1 * l2 * (c1 * s12 - s1 * c12) + l1 * l3 * (c1 * s123 - s1 * c123)) * eP;
 }
 
-Roboarm::~Roboarm(){
-
-}
+Roboarm::~Roboarm() {}
