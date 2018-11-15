46010b22efc506faabe991972b1870a7b34ecf6c
Hailey Hubbard <hchubbar@uwaterloo.ca>
2018-11-14 21:26:35 -0500

    Added struct for easy arguments and five error levels.

--- Workspace/src/console_message/src/console_message.cpp
+++ Workspace/src/console_message/src/console_message.cpp
@@ -13,8 +13,6 @@ class Console_Message
 	void sendMessage(string message, string level);
 
 	//Questions and future plans:
-	//Make a error folder in GitHub
-	//ROS error macros and/or including error level in message
 	//Compiling and running this (create a main for testing?)
 	//Create a testing procedure
 	//Next goal: link this library to other nodes
@@ -24,26 +22,60 @@ class Console_Message
 
 Console_Message::Console_Message(void){
 	//initilaize/create the publisher pub
-	ros::Publisher pub;
+	ros::Publisher info_pub, warn_pub, error_pub, fatal_pub, debug_pub;
 
-	//create topic
-	pub = node_handle.advertise<std_msgs::String>("Messages", 1);
+	//create topics
+	info_pub = node_handle.advertise<std_msgs::String>("Info", 1);
+	warn_pub = node_handle.advertise<std_msgs::String>("Warning", 1);
+	error_pub = node_handle.advertise<std_msgs::String>("Error", 1);
+	fatal_pub = node_handle.advertise<std_msgs::String>("Fatal", 1);
+	debug_pub = node_handle.advertise<std_msgs::String>("Debug", 1);
 }
 
-void Console_Message::sendMessage(string message, string level){
-	
-	//initializew message instance
+void Console_Message::sendMessage(message_t arg){
+
+	//initialize message instance
 	std_msgs::String msg;
 
-	//convert message into something?
-	msg.data = <std_msgs::String>(message);
+	switch(arg.level){
 
-	//convert level into ROS macro? ask Melvin?
-	if(level != "NULL"){
-		//set a macro? combine the message and level?
+	  case "Info" :
+		string sender_message = arg.node_sender + arg.msg;
+	  	msg.data = <std_msgs::String>(sender_message);
+		//send to Info topic
+		info_pub.publish(msg);
+		break;
+
+	  case "Warning" :
+		string sender_message = arg.node_sender + arg.msg;
+	  	msg.data = <std_msgs::String>(sender_message);
+		//send to Warning topic
+		warn_pub.publish(msg);
+		break;
+
+	  case "Error" :
+		string sender_message = arg.node_sender + arg.msg;
+	  	msg.data = <std_msgs::String>(sender_message);
+		//send to Error topic
+		error_pub.publish(msg);
+		break;
+
+	  case "Fatal" :
+		string sender_message = arg.node_sender + arg.msg;
+	  	msg.data = <std_msgs::String>(sender_message);
+		//send to Fatal topic
+		fatal_pub.publish(msg);
+		break;
+
+	  case "Debug" :
+		string sender_message = arg.node_sender + arg.msg;
+	  	msg.data = <std_msgs::String>(sender_message);
+		//send to Debug topic
+		debug_pub.publish(msg);
+		break;
+
+	//need a default case? return that the given level doesn't match what we have?
+
 	}
-	//use publisher to send error message to topic
-	pub.publish(msg);
-	
 
 }
