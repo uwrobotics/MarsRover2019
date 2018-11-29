58b36189ecdc5be507d32b67b281b33b6cf6062e
Bill DuGe <billduge11@gmail.com>
2018-11-28 21:06:35 -0500

    created subscriber node

--- Workspace/src/rover_gui/src/xbox.cpp
+++ Workspace/src/rover_gui/src/xbox.cpp
@@ -1,3 +1,31 @@
 // Code for xbox controller visualizer gui
 
-int x = 5
+#include <iostream>
+	#include <ros/ros.h>
+	
+	#include <std_msgs/Float32.h>
+	
+	
+	
+	void SubCallback(std_msgs::Float32 msg)
+	{
+	std::cout << "Received message with data: " << msg.data << std::endl;
+	}
+	
+	
+		
+	int main(int argc, char** argv)
+	{
+	//Initialize
+	ros::init(argc, argv, "sub_node");
+	ros::NodeHandle nh;
+	
+	//Create subscriber
+	
+	ros::Subscriber sub = nh.subscribe("/my_topic", 1, SubCallback);
+	
+	ros::spin();
+	
+	return 0;
+	
+	}
