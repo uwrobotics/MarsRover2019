e03deb9319189c6ce87681111d39f7385f0b22cc
OmarAlashqar <o.ashqar@outlook.com>
2018-11-28 23:50:50 -0500

    refactor and easier simulation csv creation

--- Workspace/src/ik/include/ik/Roboarm.h
+++ Workspace/src/ik/include/ik/Roboarm.h
@@ -4,47 +4,26 @@
 struct Point{
     float x;
     float y;
-    Point(const float &inX = 0.0, const float &inY = 0.0){
-        x = inX;
-        y = inY;
-    }
 };
 
 class Roboarm {
   public:
-    float velocityLink1;
-    float velocityLink2;
-    float velocityLink3;
+    float linkVelocities[3];
+    float linkAngles[3];
 
-    float angleLink1;
-    float angleLink2;
-    float angleLink3;
-
-    Point pose [4];//shoulder, elbox, wrist, ee
+    Point pose [4]; // shoulder, elbox, wrist, ee
     float eePhi;
 
-    void calc_velocities(float endEffectorVX, float endEffectorVY, float endEffectorPhi,
-                         float a1, float a2, float a3);
-    void calc_pose(bool visualize = false);
+    void calculateVelocities(float endEffector[3], float angles[3]);
+    float* calculatePose();
 
-    Roboarm(
-      float l1, float l2, float l3,
-      float a1 = 0, float a2 = 0, float a3 = 0,
-      float stepSize = 0.001
-      );
+    Roboarm(float lengths[3], float angles[3], float stepSize = 0.001);
     
     ~Roboarm();
 
   private:
-    const float alpha;
-
-    const float lengthLink1;
-    const float lengthLink2;
-    const float lengthLink3;
-    
-
+    float linkLengths[3];
+    float alpha;
 };
-
-
 
 #endif
--- Workspace/src/ik/src/main.cpp
+++ Workspace/src/ik/src/main.cpp
@@ -1,41 +1,54 @@
-#include "Roboarm.cpp"
-#include <fstream>
 #include <iostream>
-main(){
-    /*  //test basic output
-    Roboarm arm(1.2, 0.9, 0.2, 1.0, 0.5, 0.2);
-    arm.calc_velocities(1.4, 2.2, 0.0);
-    std::cout <<arm.velocityLink1<< " "<<arm.velocityLink2<< " " <<arm.velocityLink3<<std::endl;
-    arm.calc_pose();
-    std::cout <<arm.eeX<< " "<<arm.eeY<< " " <<arm.eePhi<<std::endl;
-    */
+#include <fstream>
+#include <string>
 
-    Roboarm arm(1.7, 1.0, 0.3, 0.2, -1.5, -0.2);
-    std::ofstream myfile;
-    myfile.open("build/ik/ee_crds.csv");
-    myfile << "eeX eeY,wristX wristY,elbowX elbowY,shoulderX shoulderY,\n";
+#include "Roboarm.cpp"
 
-    int count = 0;
-    while(count<2000){
+using namespace std;
 
-        count++;
-        //calculate joint velocities
-        arm.calc_velocities(0.15,0.0, 0.000, arm.angleLink1, arm.angleLink2, arm.angleLink3); //move about 10cm/s
+int main(int argc, char **argv) {
 
-        //calculate EE pose 0.01s in the future and update joint positions
-        arm.calc_pose(true);
+	if (argc != 5) {
+		cout << "Expected 4 arguments: filename, vx, vy, nSteps" << endl;
+		return 1;
+	}
 
-        //save the coordinates of the EE
-        myfile <<arm.pose[0].x << "," << arm.pose[0].y << " " <<
-                 arm.pose[1].x << "," << arm.pose[1].y << " " <<
-                 arm.pose[2].x << "," << arm.pose[2].y << " " <<
-                 arm.pose[3].x << "," << arm.pose[3].y << "\n";
-        std::cout<< "saved "<<std::endl;
+	float pi = 3.14;
 
-    }
+	string filename = argv[1];
+	float vx = atof(argv[2]);
+	float vy = atof(argv[3]);
+	int nSteps = atoi(argv[4]);
 
-    myfile.close();
+	float lengths[3] = {0.4, 0.4, 0.4};
+	float angles[3] = {pi/4, pi/4, pi/4};
+	Roboarm arm(lengths, angles);
 
+	std::ofstream myfile;
+	myfile.open(filename + ".csv");
 
+	if (!myfile.is_open()) {
+		cout << "Couldn't open file";
+		return -1;
+	}
 
+	float *currAngles = arm.calculatePose();
+	for (int count = 0; count < nSteps; count++) {
+		//calculate joint velocities
+		float velocities[3] = {vx, vy, 0};
+
+		arm.calculateVelocities(velocities, currAngles);
+		currAngles = arm.calculatePose();
+
+		myfile <<
+			arm.pose[0].x << "," << arm.pose[0].y << " " <<
+			arm.pose[1].x << "," << arm.pose[1].y << " " <<
+			arm.pose[2].x << "," << arm.pose[2].y << " " <<
+			arm.pose[3].x << "," << arm.pose[3].y << endl;
+	}
+
+	cout << "saved "<< endl;
+	myfile.close();
+
+	delete[] currAngles;
 }
