f83844326deae61a810c8458638317cbd2daf627
Hailey Hubbard <hchubbar@uwaterloo.ca>
2018-11-28 21:29:45 -0500

    Does not compile yet, CMakeLists improved

--- Workspace/src/console_message/test/test_console_message.cpp
+++ Workspace/src/console_message/test/test_console_message.cpp
@@ -1,4 +1,5 @@
-#include <console_message.h>
+#include <console_message.h> //change this
+#include <iostream>
 #include <ros/ros.h>
 #include <std_msgs/String.h>
 
@@ -7,19 +8,35 @@ int main(int argc, char **argv[]) {
 	ros::NodeHandle node_handle;
 
 	//Linking the node to the library?
-	
-	//Test 1
+
 	Console_Message msg;
-	message_t argument;
+	struct Message argument;
 	argument.node_sender = "Test_Node";
+	
+	//Test 1 - Info Topic
 	argument.msg = "Testing!";
 	argument.level = "Info";
 	msg.sendMessage(argument);
 
-	//Test 2
-	argument.node_sender = "Test_Node";
+	//Test 2 - Warning Topic
 	argument.msg = "Test2";
 	argument.level = "Warning";
 	msg.sendMessage(argument);
+
+	//Test 3 - Error Topic
+	argument.msg = "Test 3";
+	argument.level = "Error";
+	msg.sendMessage(argument);
+
+	//Test 4 - Fatal Topic
+	argument.msg = "test 4";
+	argument.level = "Fatal";
+	msg.sendMessage(argument);
+
+	//Test 5 - Debug Topic
+	argument.msg = "test 5";
+	argument.level = "Debug";
+	msg.sendMessage(argument);
+	
 
 }
