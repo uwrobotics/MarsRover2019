814cd437ea5c43c8152b61a538e765ef9b64076c
ireneojmembrere <ireneojmembrere@gmail.com>
2019-01-18 15:01:13 -0500

    final changes

--- Workspace/src/arm_interface/src/arm_motor_commands_node.cpp
+++ Workspace/src/arm_interface/src/arm_motor_commands_node.cpp
@@ -1,6 +1,7 @@
 #include <ros/ros.h>
 #include <arm_interface/commands.h>	//this will allow us to create an object that stores the information taken from the GUI robot commands
 #include <inverse_kinematics/message.h>		//this will be the object that stores all 6 motor speeds and 6 angles that are spit out of the inverse kinematics library
+#include <string> //includes string library 
 
 
 int main(int argc, char** argv) {
@@ -14,8 +15,12 @@ int main(int argc, char** argv) {
 	//loop that publishes info until the node is shut down
 	while (ros::ok()) {
 		
-		inverse_kinematics:message msgCalculatedAfterSubscription = subscriberFunction(nh, argc, argv); 
-		publisherFunction(nh, msgCalculated, freq); 
+		inverse_kinematics:message msgFromGUI = subscriberFunction(nh, argc, argv, "GUI", true); 
+		publisherFunction(nh, msgFromGUI, freq, "CAN");
+
+
+		inverse_kinematics:message msgCalculatedAfterSubscription = subscriberFunction(nh, argc, argv, "CAN", false); 
+		publisherFunction(nh, msgCalculated, freq, "GUI"); 
 
 
 		//wait until the next iteration call
@@ -23,16 +28,27 @@ int main(int argc, char** argv) {
 	}
 }
 
-inverse_kinematics::message messageRecieved(const arm_interface::commands msg) {
+inverse_kinematics::message messageRecievedFromGUI(const arm_interface::commands msg) {
 	return inverseK(msg);
 }
 
-inverse_kinematics::message subscriberFunction (ros::NodeHandle nh, int argc, char** argv) {
+inverse_kinematics::message messageRecievedFromIK(const arm_interface::commands msg) {
+	return msg; 
+}
+
+inverse_kinematics::message subscriberFunction (ros::NodeHandle nh, int argc, char** argv, std::string topic, std::bool SendToIK) {
 	//initializing the node
 	ros::init(argc, argv, "subscribe_arm_motor_commands");
+
+	if (SendToIK) {
+		//create a subscriber object
+		ros::Subscriber sub = nh.subscribe(topic, 10, &messageFromGUI);	
+	}
 	
-	//create a subscriber object
-	ros::Subscriber sub = nh.subscribe("CAN", 10, &messageRecieved);
+	else {
+		//create a subscriber object
+		ros::Subscriber sub = nh.subscribe(topic, 10, &messageRecievedFromIK);		
+	}
 
 	return ros::spin();		
 }
@@ -41,9 +57,9 @@ inverse_kinematics::message inverseK(arm_interface::commands msg) {
 	return inverse_kinematics::message.ik(msg);	//where ik is the method in the library that returns optimal motor speeds and angles
 }
 
-void pubisherFunction (ros::NodeHandle nh, inverse_kinematics::message msg, int freq){
+void pubisherFunction (ros::NodeHandle nh, inverse_kinematics::message msg, int freq, std::string topic){
 	//create publisher object
-	ros::Publisher pub_user_to_can = nh.advertise<inverse_kinematics::message>("CAN", 1000);
+	ros::Publisher pub_user_to_can = nh.advertise<inverse_kinematics::message>(topic, 1000);
 	ros::Rate rate(freq);
 
 	//publish message object and send message to rosout with details
