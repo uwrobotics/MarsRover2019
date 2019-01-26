fd0ac6fbedac4aaa4bca5058783ff0005443bb50
Tom Meredith <tommeredith14@gmail.com>
2019-01-26 00:07:37 -0500

    modified robotech code for 3 controllers

--- Workspace/src/roboteq-indigo-devel/roboteq_driver/src/driver.cpp
+++ Workspace/src/roboteq-indigo-devel/roboteq_driver/src/driver.cpp
@@ -33,45 +33,64 @@ int main(int argc, char **argv) {
   ros::init(argc, argv, "~");
   ros::NodeHandle nh("~");
 
-  std::string port = "/dev/ttyUSB0";
+  std::string port_l = "/dev/ttyUSB0";
+  std::string port_r = "/dev/ttyUSB1";
+  std::string port_b = "/dev/ttyUSB2";
   int32_t baud = 115200;
-  nh.param<std::string>("port", port, port);
+  nh.param<std::string>("port_left", port_l, port_l);
+  nh.param<std::string>("port_right", port_r, port_r);
+  nh.param<std::string>("port_back", port_b, port_b);
   nh.param<int32_t>("baud", baud, baud);
 
   // Interface to motor controller.
-  roboteq::Controller controller(port.c_str(), baud);
+  roboteq::Controller controller_left(port_l.c_str(), baud);
+  roboteq::Controller controller_right(port_r.c_str(), baud);
+  roboteq::Controller controller_back(port_b.c_str(), baud);
 
   // Setup channels.
   if (nh.hasParam("channels")) {
     XmlRpc::XmlRpcValue channel_namespaces;
     nh.getParam("channels", channel_namespaces);
     ROS_ASSERT(channel_namespaces.getType() == XmlRpc::XmlRpcValue::TypeArray);
+    ROS_ASSERT(channel_namespaces.size() == 6)
     for (int i = 0; i < channel_namespaces.size(); ++i) 
     {
       ROS_ASSERT(channel_namespaces[i].getType() == XmlRpc::XmlRpcValue::TypeString);
-      controller.addChannel(new roboteq::Channel(1 + i, channel_namespaces[i], &controller));
+//      controller.addChannel(new roboteq::Channel(1 + i, channel_namespaces[i], &controller));
     }
+    controller_left.addChannel(new roboteq::Channel(1 + 0, channel_namespaces[0], &controller_left));
+    controller_left.addChannel(new roboteq::Channel(1 + 1, channel_namespaces[1], &controller_left));
+    controller_right.addChannel(new roboteq::Channel(1 + 0, channel_namespaces[2], &controller_right));
+    controller_right.addChannel(new roboteq::Channel(1 + 1, channel_namespaces[3], &controller_right));
+    controller_back.addChannel(new roboteq::Channel(1 + 0, channel_namespaces[4], &controller_back));
+    controller_back.addChannel(new roboteq::Channel(1 + 1, channel_namespaces[5], &controller_back));
     ROS_INFO("MULTIPLE CHANNELS");
   } else {
     // Default configuration is a single channel in the node's namespace.
-    controller.addChannel(new roboteq::Channel(1, "~", &controller));
+    controller_left.addChannel(new roboteq::Channel(1, "~", &controller_left));
     ROS_INFO("SINGLE CHANNEL");
   } 
 
   // Attempt to connect and run.
   while (ros::ok()) {
-    ROS_DEBUG("Attempting connection to %s at %i baud.", port.c_str(), baud);
-    controller.connect();
-    if (controller.connected()) {
+    ROS_DEBUG("Attempting connection to %s at %i baud.", port_l.c_str(), baud);
+    controller_left.connect();
+    ROS_DEBUG("Attempting connection to %s at %i baud.", port_r.c_str(), baud);
+    controller_right.connect();
+    ROS_DEBUG("Attempting connection to %s at %i baud.", port_b.c_str(), baud);
+    controller_back.connect();
+    if (controller_left.connected() && controller_right.connected() && controller_back.connected()) {
       ros::AsyncSpinner spinner(1);
       spinner.start();
       while (ros::ok()) {
-        controller.spinOnce();
+        controller_left.spinOnce();
+        controller_right.spinOnce();
+        controller_back.spinOnce();
       }
       spinner.stop();
     } else {
       ROS_DEBUG("Problem connecting to serial device.");
-      ROS_ERROR_STREAM_ONCE("Problem connecting to port " << port << ". Trying again every 1 second.");
+      ROS_ERROR_STREAM_ONCE("Problem connecting to ports " << port_l << " or " << port_r << " or " << port_b << ". Trying again every 1 second.");
       sleep(1);
     }  
   }
--- Workspace/src/twist2roboteq/src/twist2roboteq_node.cpp
+++ Workspace/src/twist2roboteq/src/twist2roboteq_node.cpp
@@ -3,8 +3,12 @@
 #include "twist2roboteq_node.h"
 #include "rover_physical_param.h"
 
-ros::Publisher left_pub;
-ros::Publisher right_pub;
+ros::Publisher left_front_pub;
+ros::Publisher left_middle_pub;
+ros::Publisher left_back_pub;
+ros::Publisher right_front_pub;
+ros::Publisher right_middle_pub;
+ros::Publisher right_back_pub;
 
 
 const float SCALE = 1000;  // max value for roboteq
@@ -64,32 +68,40 @@ int main(int argc, char** argv)
 	ros::init(argc, argv, "twist2roboteq_node");
 	ros::NodeHandle n;
 	ros::Subscriber sub = n.subscribe("cmd_vel", 100, twistCallback);
-	left_pub = n.advertise<roboteq_msgs::Command&>("/left/cmd",1);
-	right_pub = n.advertise<roboteq_msgs::Command&>("/right/cmd",1);
+	left_front_pub = n.advertise<roboteq_msgs::Command&>("/left_front/cmd",1);
+  left_middle_pub = n.advertise<roboteq_msgs::Command&>("/left_middle/cmd",1);
+  left_back_pub = n.advertise<roboteq_msgs::Command&>("/left_back/cmd",1);
+	right_front_pub = n.advertise<roboteq_msgs::Command&>("/right_front/cmd",1);
+  right_middle_pub = n.advertise<roboteq_msgs::Command&>("/right_middle/cmd",1);
+  right_back_pub = n.advertise<roboteq_msgs::Command&>("/right_back/cmd",1);
 
-    double alpha = 0.95;
+  double alpha = 0.95;
 
-    roboteq_msgs::Command left_command;
-    roboteq_msgs::Command right_command;
+  roboteq_msgs::Command left_command;
+  roboteq_msgs::Command right_command;
 
-    ros::Rate loop_rate(60);
-    while(ros::ok()) {
-        loop_rate.sleep(); //Maintain the loop rate
-        ros::spinOnce();   //Check for new messages
+  ros::Rate loop_rate(60);
+  while(ros::ok()) {
+    loop_rate.sleep(); //Maintain the loop rate
+    ros::spinOnce();   //Check for new messages
 
 
-        left_command.mode = MODE_VELOCITY;
-        right_command.mode = MODE_VELOCITY;
+    left_command.mode = MODE_VELOCITY;
+    right_command.mode = MODE_VELOCITY;
 
-        left_command.setpoint = left * alpha + left_command.setpoint * (1 - alpha);
-        right_command.setpoint = right * alpha + right_command.setpoint * (1 - alpha);
+    left_command.setpoint = left * alpha + left_command.setpoint * (1 - alpha);
+    right_command.setpoint = right * alpha + right_command.setpoint * (1 - alpha);
 
-        left_pub.publish(left_command);
-        right_pub.publish(right_command);
-    }
+    left_front_pub.publish(left_command);
+    left_middle_pub.publish(left_command);
+    left_back_pub.publish(left_command);
+    right_front_pub.publish(right_command);
+    right_middle_pub.publish(right_command);
+    right_back_pub.publish(right_command);
+  }
 
-    ROS_INFO("left: %f right: %f\r\n\r\n", left, right);
-	
+  ROS_INFO("left: %f right: %f\r\n\r\n", left, right);
+
 	ros::spin();
 	return 0;
 }
