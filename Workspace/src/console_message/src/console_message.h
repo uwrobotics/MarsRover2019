5b008fd80947d038af4ff22012fe61bdcc45842e
Melvin Wang <melvin.mc.wang@gmail.com>
2019-01-28 22:14:32 -0800

    Revert "Added struct for easy arguments and five error levels."
    This reverts commit e44b8ad2acc84c54f40002308bb4019ddd67c38d.

--- Workspace/src/console_message/src/console_message.h
+++ Workspace/src/console_message/src/console_message.h
@@ -3,21 +3,8 @@
 class Console_Message{
 
 public:
-
-	struct message_t{
-	  string node_sender;
-	  string msg;
-	  string level;
-	};
-
-	//User notes - there are five error levels (case matters):
-	//Info - General useful information to log, or be aware about
-	//Warning - Bringing attention to system issues that may produce behaviourial changes, but were corrected/compensated
-	//Error - Something fatally wrong happened to the operation, or a component, but the system is still running overall
-	//Fatal - The entire system (node, motor, arm, ect) is permanently offline
-	//Debug - Info that team members other than the software team could use/would like to know
 	
 	Console_Message();
-	void sendMessage(message_t arg);
+	void sendMessage(string message, string level);
 
 };
--- Workspace/src/console_message/test/test_console_message.cpp
+++ Workspace/src/console_message/test/test_console_message.cpp
@@ -10,16 +10,9 @@ int main(int argc, char **argv[]) {
 	
 	//Test 1
 	Console_Message msg;
-	message_t argument;
-	argument.node_sender = "Test_Node";
-	argument.msg = "Testing!";
-	argument.level = "Info";
-	msg.sendMessage(argument);
+	msg.sendMessage("Testing!", "NULL");
 
 	//Test 2
-	argument.node_sender = "Test_Node";
-	argument.msg = "Test2";
-	argument.level = "Warning";
-	msg.sendMessage(argument);
+	msg.sendMessage("Testing levels", "Warning");
 
 }
