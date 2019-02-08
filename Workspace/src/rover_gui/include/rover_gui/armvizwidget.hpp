ce1da0e96965d772626b90e4b81c7f99ef07c407
qinyang-bao <q7bao@edu.uwaterloo.ca>
2019-02-07 20:48:38 -0500

    something

--- Workspace/src/rover_gui/include/rover_gui/armvizwidget.hpp
+++ Workspace/src/rover_gui/include/rover_gui/armvizwidget.hpp
@@ -1,22 +1,24 @@
 #ifndef ARMVIZWIDGET_HPP
 #define ARMVIZWIDGET_HPP
 
+// remember to change to correct topic name
+#define ARM_POSE_TOPIC "/localization/pose_arm"
+
 #include <QWidget>
 
 namespace Ui {
 class armvizwidget;
 }
 
-class armvizwidget : public QWidget
-{
-    Q_OBJECT
+class armvizwidget : public QWidget{
+	Q_OBJECT
 
-public:
-    explicit armvizwidget(QWidget *parent = nullptr);
-    ~armvizwidget();
+	public:
+	    explicit armvizwidget(QWidget *parent = nullptr);
+	    ~armvizwidget();
 
-private:
-    Ui::armvizwidget *ui;
+	private:
+	    Ui::armvizwidget *ui;
 };
 
 #endif // ARMVIZWIDGET_HPP
--- Workspace/src/rover_gui/src/armvizwidget.cpp
+++ Workspace/src/rover_gui/src/armvizwidget.cpp
@@ -1,14 +1,13 @@
 #include "armvizwidget.hpp"
 #include "ui_armvizwidget.h"
 
-armvizwidget::armvizwidget(QWidget *parent) :
-    QWidget(parent),
-    ui(new Ui::armvizwidget)
+armvizwidget::armvizwidget(QWidget *parent) : 
+QWidget(parent), 
+ui(new Ui::armvizwidget) 
 {
     ui->setupUi(this);
 }
 
-armvizwidget::~armvizwidget()
-{
+armvizwidget::~armvizwidget() {
     delete ui;
 }
