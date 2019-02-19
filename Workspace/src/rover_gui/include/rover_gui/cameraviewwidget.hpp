2eea2052ccd5774168aa3de498657e97454d0f2f
Billy Kim <ih4kim@edu.uwaterloo.ca>
2019-02-18 22:21:38 -0500

    ih4kim

--- Workspace/src/rover_gui/include/rover_gui/cameraviewwidget.hpp
+++ Workspace/src/rover_gui/include/rover_gui/cameraviewwidget.hpp
@@ -1,8 +1,13 @@
 #ifndef CAMERAVIEWWIDGET_HPP
 #define CAMERAVIEWWIDGET_HPP
 
-#include <QWidget>
+#ifndef Q_MOC_RUN
 
+	#include <QWidget>
+	#include "sensor_msgs/Image.h"
+	#include <ros/ros.h>
+
+#endif
 namespace Ui {
 class CameraViewWidget;
 }
@@ -13,9 +18,12 @@ class CameraViewWidget : public QWidget {
 public:
   explicit CameraViewWidget(QWidget *parent = nullptr);
   ~CameraViewWidget();
+  void subscribe(ros::NodeHandle &guiHandle);
+  void imageCallback(const sensor_msgs::Image::ConstPtr& msg);
 
 private:
   Ui::CameraViewWidget *ui;
+  ros::Subscriber sub;
 };
 
 #endif // CAMERAVIEWWIDGET_HPP
--- Workspace/src/rover_gui/src/cameraviewwidget.cpp
+++ Workspace/src/rover_gui/src/cameraviewwidget.cpp
@@ -1,5 +1,6 @@
 #include "cameraviewwidget.hpp"
 #include "ui_cameraviewwidget.h"
+#include "sensor_msgs/Image.h"
 
 CameraViewWidget::CameraViewWidget(QWidget *parent)
     : QWidget(parent), ui(new Ui::CameraViewWidget) {
@@ -7,3 +8,17 @@ CameraViewWidget::CameraViewWidget(QWidget *parent)
 }
 
 CameraViewWidget::~CameraViewWidget() { delete ui; }
+
+void CameraViewWidget::subscribe(ros::NodeHandle &guiHandle)
+{
+	sub = guiHandle.subscribe("cameratopic",1000, &CameraViewWidget::imageCallback, this);
+}
+
+void CameraViewWidget::imageCallback(const sensor_msgs::Image::ConstPtr& msg)
+{
+	QImage temp(&(msg->data[0]), msg->width, msg->height, QImage::Format_RGB888);
+	QImage image;
+	image = temp;
+	ui->label->setPixmap(QPixmap::fromImage(image));
+}
+
