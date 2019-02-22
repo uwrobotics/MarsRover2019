034ec793e75ecbb3b56686be1c1c03687b44f77d
Sidharth Bajaj <2bajajsid@gmail.com>
2019-02-22 12:27:17 -0500

    Undefined reference error

--- Workspace/src/arm_interface/src/arm_motor_commands_node.cpp
+++ Workspace/src/arm_interface/src/arm_motor_commands_node.cpp
@@ -5,22 +5,17 @@
 #include <std_msgs/Float64MultiArray.h>
 #include <arm_node/Custom_msg.h>
 
-struct msg_from_topics {		//not needed, can remove it when custom ros message implemented
-	bool ik_status; 
-	std_msgs::Float64MultiArray data_points; 
-};
-
 class InitObjects {
 	private:
-	ros::NodeHandle nh;
-	ros::Publisher pub_user_to_GUI;
-	ros::Publisher pub_user_to_CAN;
-	std_msgs::Int32 freq;
+	static ros::NodeHandle nh;  
+        static ros::Publisher pub_user_to_GUI;
+	static ros::Publisher pub_user_to_CAN;   
+        static int freq;
 
 	public:
-	construct() {
+	InitObjects() {
 		pub_user_to_GUI =  nh.advertise<std_msgs::Float64MultiArray>("GUI", 1000); 
-       		pub_user_to_CAN =  nh.advertise<std_msgs::Float64MultiArray>("CAN", 1000);
+       		pub_user_to_CAN =  nh.advertise<arm_node::Custom_msg>("CAN", 1000);
 		freq = 10; 
 	}
 
@@ -34,21 +29,21 @@ class InitObjects {
 	static ros::NodeHandle getNodeHandlerObject() {
 		return nh; 
 	}	
-	static std_msgs::Int32 getFreq() {
+	static int getFreq() {
 		return freq; 
 	}	
-}
+};
 
 
 // publisher function consumes the parameters required to create the publisher object, the frequency at which publishing is carried out,
 // and the topic which needs to be published to. 
-void publisherFunctionToCAN (msg_from_topics msg){
+void publisherFunctionToCAN (arm_node::Custom_msg msg){
 	//create publisher object
-	ros::Rate rate(InitObjects.getFreq()); 
+	ros::Rate rate(InitObjects::getFreq()); 
 	
-	if (true) {
+	if (msg.ik_status == false) {
 	//this should only run when ik_status is false, but this will be fixed when the inverseKinematicsLibrary is ready to be implemented i.e. "if (!msg.ik_status)"
-	InitObjects.getPubCAN().publish(msg);
+	InitObjects::getPubCAN().publish(msg);
 	}	
 	else {
 	//this should pass the msg into the inverse kinematics library and then publish the returned structure
@@ -58,16 +53,16 @@ void publisherFunctionToCAN (msg_from_topics msg){
 
 void publisherFunctionToGUI (std_msgs::Float64MultiArray msg){
 	//create publisher object
-	ros::Rate rate(InitObjects.getFreq()); 
+	ros::Rate rate(InitObjects::getFreq()); 
 
 	//publish message object and send message to rosout with details
-	InitObjects.getPubGUI().publish(msg);
+	InitObjects::getPubGUI().publish(msg);
 	
 }
 
 // messageRecievedFromGUI return the message that is recieved from the Inverse Kinematics library after it passes the message parameter into the library
 // Not sure if this library exists or not yet
-void messageReceivedFromGUI(const msg_from_topics values) {
+void messageReceivedFromGUI(const arm_node::Custom_msg values) {
 	
 	publisherFunctionToCAN(values);
 }
@@ -79,10 +74,11 @@ void messageReceivedFromCAN(const std_msgs::Float64MultiArray values) {
 }
 
 // subscriberFunction is passed into the necessary parameters required to initiaize the node and a boolean that determines if the message that is recieved from the topic
+
 // needs to be passed into the Inverse Kinematics library or has it returned from it and needs to be returned as it is 
 void subscriberFunctionFromGUI (int argc, char** argv) {
 	
-	ros::Subscriber sub = InitObjects.getNodeHandlerObject().subscribe("GUI", 10, &messageReceivedFromGUI);
+	ros::Subscriber sub = InitObjects::getNodeHandlerObject().subscribe("GUI", 10, &messageReceivedFromGUI);
 
 	ros::spinOnce();		
 }
@@ -92,7 +88,7 @@ void subscriberFunctionFromCAN (int argc, char** argv) {
 	ros::init(argc, argv, "subscribe_arm_motor_commands");
 
 
-	ros::Subscriber sub = InitObjects.getNodeHandlerObject().subscribe("CAN", 10, &messageReceivedFromCAN);
+	ros::Subscriber sub = InitObjects::getNodeHandlerObject().subscribe("CAN", 10, &messageReceivedFromCAN);
 
 	ros::spinOnce();		
 }
