0395c458808431f115acf824b46847d4bbfe6562
Ben Duo <ben.duo99@gmail.com>
2019-03-21 20:36:34 -0400

    subscribers done. unsure about utm callback function Pose2DConstPtr

--- Workspace/src/direction_overlay/direction_node.cpp
+++ Workspace/src/direction_overlay/direction_node.cpp
@@ -2,6 +2,7 @@
 #include <ros/ros.h>
 #include <opencv2/core.hpp>
 #include <sensor_msgs/NavSatFix.h>
+#include <sensor_msgs/Image.h>
 #include <geometry_msgs/Pose2D.h>
 
 
@@ -10,14 +11,20 @@ using namespace std;
 
 sensor_msgs::NavSatFixConstPtr pNavSatOrigin = nullptr;
 
-void NavSatCallback(sensor_msgs::NavSatFixConstPtr msg) { pNavSatOrigin = msg; }
+void utmPubCallback(geometry_msgs::Pose2DConstPtr msg) { 
+	curr = msg; 
+}
+
+void rgbImageCallback(const sensor_msgs::Image::ConstPtr& msg) {
+	ROS_INFO("RGB Image received from left ZED camera - Size: %d%d", msg->width, msg->height);
+}
 
 int main(int argc, char **argv) {
 	ros::init(argc, argv, "direction_node");
-	ros::NodeHandle n;
+	ros::NodeHandle nh;
 
-	ros::Subscriber utmPub = nh.subscribe<geometry_msgs::Pose2D>("/localization/pose_utm", 1);
-	ros::Subscriber sub = zed/rgb/image_rect_color.subscribe("ZEDimg", 25)
+	ros::Subscriber utmSub = nh.subscribe<geometry_msgs::Pose2D>("/localization/pose_utm", 1 utmPubCallback);
+	ros::Subscriber imgSub = nh.subscribe<>("zed/rgb/image_rect_color", 25, rgbImageCallback)
 	ros::NodeHandle zed/rgb/image_rect_color;
 
 	if (!ros::ok()) {
@@ -27,20 +34,20 @@ int main(int argc, char **argv) {
 
 	//need to determine rate at which we want to update co-ords
 
-	
+	ros::Rate loopRate(10);
 
-	int latA, longA, latB, longB, X, Y, theta = 0;
+	double latA, longA, latB, longB, X, Y, theta = 0;
 
-	int latA = ; //average of latitude of point A
-	int longA = ; //average of longitude of point A
-	int latB = ; //average of latitude of point B
-	int longB = ; //average of longitude of point B
+	double latA = ; 
+	double longA = ; 
+	double latB = ; 
+	double longB = ;
 
-	int X = cos(latB) * sin(longB-longA);
-	int Y = cos(latA) * sin(latB) - sin(latA) * cos(latB) * cos(longB-longA);
+	double X = cos(latB) * sin(longB-longA);
+	double Y = cos(latA) * sin(latB) - sin(latA) * cos(latB) * cos(longB-longA);
 
 	//this is the bearing in degrees, 0 is north, 90 is east, 180 is south, 270 is west
-	int theta = atan2(X, Y);
+	double theta = atan2(X, Y);
 
 
 
