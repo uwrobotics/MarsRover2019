2240264ad08ae03cb60fb18fd83fce4a30feb2f4
Tom Meredith <tommeredith14@gmail.com>
2019-03-30 20:00:15 -0400

    changed ik to use eigen, and added position ik

--- Workspace/src/ik/src/Roboarm.cpp
+++ Workspace/src/ik/src/Roboarm.cpp
@@ -1,13 +1,18 @@
 #include <cmath>
 #include <iostream>
 
-#include "../include/ik/Roboarm.h"
+#include "Roboarm.h"
+#include <Eigen/Core>
+#include <Eigen/Dense>
 #include <ros/ros.h>
 
 using namespace std;
+using namespace Eigen;
 
-Roboarm::Roboarm(double lengths[3], double angles[3], double stepSize)
-    : alpha(stepSize) {
+Roboarm::Roboarm(double lengths[3], double angles[3], double normTol,
+                 double angleTol, double stepSize)
+    : alpha(stepSize), mAngleTolerance(angleTol),
+      mNormTolerance2(normTol * normTol) {
 
   for (int i = 0; i < 3; i++) {
     linkVelocities[i] = 0;
@@ -16,12 +21,12 @@ Roboarm::Roboarm(double lengths[3], double angles[3], double stepSize)
   }
 }
 
-bool Roboarm::calculateVelocities(double endEffector[3], double angles[3],
+bool Roboarm::calculateVelocities(double endEffectorVel[3], double curAngles[3],
                                   bool limits) {
 
   // prevent elbow from fully extending (0 radian to shoulder link) or
   // determinant will be 0.
-  if (abs(angles[1]) < 0.0872665) { // 5 degree tolerance
+  if (abs(curAngles[1]) < 0.0872665) { // 5 degree tolerance
     cout << "Inverse jacobian unsolved. Halting motion";
     linkVelocities[0] = 0.0;
     linkVelocities[1] = 0.0;
@@ -29,14 +34,14 @@ bool Roboarm::calculateVelocities(double endEffector[3], double angles[3],
     return false;
   }
 
-  double eeX = endEffector[0];
-  double eeY = endEffector[1];
-  double eePhi = endEffector[2];
+  double eeX = endEffectorVel[0];
+  double eeY = endEffectorVel[1];
+  double eePhi = endEffectorVel[2];
   ROS_INFO("ex: %f, ey: %f, ePhi: %f", eeX, eeY, eePhi);
 
-  linkAngles[0] = angles[0];
-  linkAngles[1] = angles[1];
-  linkAngles[2] = angles[2];
+  linkAngles[0] = curAngles[0];
+  linkAngles[1] = curAngles[1];
+  linkAngles[2] = curAngles[2];
   ROS_INFO("link angles: %f, %f, %f", linkAngles[0], linkAngles[1],
            linkAngles[2]);
 
@@ -53,6 +58,31 @@ bool Roboarm::calculateVelocities(double endEffector[3], double angles[3],
   double l2 = linkLengths[1];
   double l3 = linkLengths[2];
 
+  Matrix3d J;
+
+  J << -(l1 * s1 + l2 * s12 + l3 * s123), -(l2 * s12 + l3 * s123), -l3 * s123,
+      (l1 * c1 + l2 * c12 + l3 * c123), (l2 * c12 + l3 * c123), l3 * c123, 1, 1,
+      1;
+
+  Vector3d desVel;
+  desVel << eeX, eeY, eePhi;
+
+  Vector3d jointVel = J.colPivHouseholderQr().solve(desVel);
+
+  if (!(J * jointVel).isApprox(desVel, 0.01)) {
+    linkVelocities[0] = 0.0;
+    linkVelocities[1] = 0.0;
+    linkVelocities[2] = 0.0;
+  } else {
+    linkVelocities[0] = jointVel(0);
+    linkVelocities[1] = jointVel(1);
+    linkVelocities[2] = jointVel(2);
+    ROS_INFO("link vel: %f", linkVelocities[0]);
+    ROS_INFO("link vel: %f", linkVelocities[1]);
+    ROS_INFO("link vel: %f", linkVelocities[2]);
+  }
+
+  /*
   linkVelocities[0] = (l2 * c12 * eeX) + (l2 * s12 * eeY) +
                       (l2 * l3 * (c12 * s123 - s12 * c123) * eePhi);
   ROS_INFO("link vel: %f", linkVelocities[0]);
@@ -68,7 +98,7 @@ bool Roboarm::calculateVelocities(double endEffector[3], double angles[3],
       (l1 * l2 * (c1 * s12 - s1 * c12) + l1 * l3 * (c1 * s123 - s1 * c123)) *
           eePhi;
   ROS_INFO("link vel: %f", linkVelocities[2]);
-
+*/
   //  halt if illegal motion attempted
   if (limits == true) {
     for (int i = 0; i < 3; i++) {
@@ -83,6 +113,100 @@ bool Roboarm::calculateVelocities(double endEffector[3], double angles[3],
     }
   }
   return true;
+}
+
+// static Vector3d CalculateEEPose(Vector3d jointPos, double l[3])
+//{
+//
+//}
+
+bool Roboarm::calculatePositionIK(double endEffectorPos[3], double curAngles[3],
+                                  double anglesOut[3],
+                                  bool limits /*= false*/) {
+  anglesOut[0] = 0;
+  anglesOut[1] = 0;
+  anglesOut[2] = 0;
+
+  // prevent elbow from fully extending (0 radian to shoulder link) or
+  // determinant will be 0.
+  if (abs(curAngles[1]) < 0.0872665) { // 5 degree tolerance
+    cout << "Inverse jacobian unsolved. Halting motion";
+    return false;
+  }
+
+  double eeX = endEffectorPos[0];
+  double eeY = endEffectorPos[1];
+  double eePhi = endEffectorPos[2];
+  // ROS_INFO("ex: %f, ey: %f, ePhi: %f", eeX, eeY, eePhi);
+
+  // ROS_INFO("link angles: %f, %f, %f", curAngles[0], curAngles[1],
+  //         curAngles[2]);
+
+  double l1 = linkLengths[0];
+  double l2 = linkLengths[1];
+  double l3 = linkLengths[2];
+
+  // Take an iterative approach to solve for the desired end position
+  Vector3d desPos;
+  desPos << eeX, eeY, eePhi;
+
+  Vector3d curJoints;
+  curJoints << curAngles[0], curAngles[1], curAngles[2];
+
+  bool converged = false;
+  for (int iteration = 0; iteration < 1000; iteration++) {
+    // std::cout << "IT " << iteration << ": " << curJoints(0) << ", " <<
+    // curJoints(1) << ", " << curJoints(2) << std::endl;
+
+    double c1 = cos(curJoints(0));
+    double s1 = sin(curJoints(0));
+
+    double c12 = cos(curJoints(0) + curJoints(1));
+    double s12 = sin(curJoints(0) + curJoints(1));
+
+    double c123 = cos(curJoints(0) + curJoints(1) + curJoints(2));
+    double s123 = sin(curJoints(0) + curJoints(1) + curJoints(2));
+
+    Vector3d curPos;
+    curPos << (l1 * c1 + l2 * c12 + l3 * c123),
+        (l1 * s1 + l2 * s12 + l3 * s123), (curJoints.sum());
+
+    Vector3d deltaPos = desPos - curPos;
+
+    // std::cout << "Cur   pose: " << curPos << std::endl;
+    // std::cout << "Delta pose: " << deltaPos << std::endl;
+    // std::cout << "Mag2: " << deltaPos.squaredNorm() << std::endl;
+
+    if (deltaPos.squaredNorm() < mNormTolerance2 &&
+        abs(deltaPos(2)) < mAngleTolerance) {
+      // std::cout << "converged" << std::endl;
+      converged = true;
+      break;
+    }
+
+    // Iteration: compute jacobian, solve approx joint change, update
+    Matrix3d J;
+
+    J << -(l1 * s1 + l2 * s12 + l3 * s123), -(l2 * s12 + l3 * s123), -l3 * s123,
+        (l1 * c1 + l2 * c12 + l3 * c123), (l2 * c12 + l3 * c123), l3 * c123, 1,
+        1, 1;
+
+    Vector3d jointChange = J.colPivHouseholderQr().solve(deltaPos); // Slow
+    // Vector3d jointChange = J.transpose() * deltaPos; // Faster, doesn't seem
+    // to work as promised
+
+    // std::cout << "Joint change: " << jointChange << std::endl;
+
+    curJoints = curJoints + jointChange * alpha;
+  }
+
+  if (converged) {
+    anglesOut[0] = curJoints(0);
+    anglesOut[1] = curJoints(1);
+    anglesOut[2] = curJoints(2);
+  }
+
+  return converged;
 }
 
 double *Roboarm::calculatePose() {
